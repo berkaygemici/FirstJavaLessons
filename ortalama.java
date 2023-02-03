a8dfbab2d6885c286ea9d84de7f3dde5dfafad2d
Berkay Gemici <berkayygemici@gmail.com>
2023-02-04 00:50:37 +0300

    First Commit

---
+++ ortalama.java
@@ -0,0 +1,11 @@
+package giris;
+import java.util.Scanner;
+
+public class ortalama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Matematik notunuzu giriniz");
+        int matematik = input.nextInt();
+        System.out.println(matematik);
+    }
+}
