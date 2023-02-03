59b5bd918ff75ab1921c68b1b4fff7bbfc41db31
Berkay Gemici <berkayygemici@gmail.com>
2023-02-04 01:07:10 +0300

    style dosyasi eklendi

---
+++ operators.java
@@ -0,0 +1,15 @@
+package giris;
+
+public class operators {
+    public static void main(String[] args) {
+        int a=5,b=6,c=5;
+        boolean kosul1 = a==c;
+        boolean kosul2 = a >=b;
+        boolean sonuc = kosul1|| kosul2;
+        System.out.println(sonuc);
+
+        String str = sonuc ? "Doğru" : "Değil"; // 1:0
+        System.out.println(str);
+
+    }
+}
