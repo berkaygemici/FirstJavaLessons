38e516ea6a6b486c1220c166f27107f0b4ca7674
Berkay Gemici <berkayygemici@gmail.com>
2023-02-04 01:07:55 +0300

    tum dersler tek int altina toplanildi

--- ortalama.java
+++ ortalama.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class ortalama {
     public static void main(String[] args) {
+        int matematik, fizik, kimya, turkce, tarih, muzik;
         Scanner input = new Scanner(System.in);
         System.out.print("Matematik notunuzu giriniz");
-        int matematik = input.nextInt();
+        matematik = input.nextInt();
         System.out.println(matematik);
     }
 }
