0ab25b0d1a5339ef60ebe5b9ef7efad1305dd3bf
Berkay <e210506002@stud.tau.edu.tr>
2023-02-04 17:46:10 +0300

    ortalama bulucu tamamlandi, sinif gecme notu belirlendi

--- ortalama.java
+++ ortalama.java
@@ -7,16 +7,22 @@ public class ortalama {
         Scanner input = new Scanner(System.in);
         System.out.print("Matematik notunuzu giriniz");
         matematik = input.nextInt();
+        System.out.print("Fizik notunuzu giriniz");
         fizik = input.nextInt();
+        System.out.print("Kimya notunuzu giriniz");
         kimya = input.nextInt();
+        System.out.print("Türkçe notunuzu giriniz");
         turkce = input.nextInt();
+        System.out.print("Tarih notunuzu giriniz");
         tarih = input.nextInt();
+        System.out.print("Müzik notunuzu giriniz");
         muzik = input.nextInt();
-        //github hesap denemesi
-        // bir deneme daha
-        //lutfensondeneme
-        //ahahahha
-        //lalalalalla
+        System.out.println("Ortalama hesaplaniyor.....");
+        double ortalama = (matematik + fizik + kimya + turkce + tarih + muzik)/6.0;
+        System.out.println("Ortalamanız:" + ortalama);
+        String son = ortalama > 60 ? "Gecti" : "Kaldi";
+        System.out.println(son);
+
 
     }
 }
