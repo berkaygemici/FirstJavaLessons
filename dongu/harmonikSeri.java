6d53d3b545c04998fc590c5c54e982a6f6bdd849
Berkay <e210506002@stud.tau.edu.tr>
2023-02-10 21:50:49 +0300

    harmonik seri hesaplayıcı

---
+++ dongu/harmonikSeri.java
@@ -0,0 +1,17 @@
+package giris.dongu;
+
+import java.util.Scanner;
+
+public class harmonikSeri {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen N sayısını giriniz:");
+        int nNumber = input.nextInt();
+        double harmonikSeri =0;
+        for (int i = 1; i <= nNumber;i++) {
+            harmonikSeri += (1.0/i);
+        }
+        System.out.println(harmonikSeri);
+    }
+
+}
